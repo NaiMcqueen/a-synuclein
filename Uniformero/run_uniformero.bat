7730b25c5ef496a55eb3fc80b300db6c54a8c6dc
NaiMcqueen <naiaraxime@gmail.com>
2025-06-10 14:47:28 -0300

    eliminar .bat

--- Uniformero/run_uniformero.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Cambia al directorio donde están los scripts y archivos
-cd /d C:\Users\Naiar\Naidocking\Uniformero
-
-REM Ejecuta pymol con el script y el archivo de entrada
-"C:\ProgramData\pymol\Scripts\pymol.exe" -cq scripts\rms_script.py -- outputs\tuarchivo_out.pdbqt
