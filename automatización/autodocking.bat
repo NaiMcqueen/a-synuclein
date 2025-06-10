a2c12e4564437ec5b905d33294582b08b6dd5114
NaiMcqueen <naiaraxime@gmail.com>
2025-06-10 15:42:25 -0300

    prueba 3

--- automatización/autodocking.bat
+++ automatización/autodocking.bat
@@ -1,19 +1,22 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "configDir=C:\Users\naiar\Naidocking\configs\"
-set "ligandDir=C:\Users\naiar\Naidocking\ligands\"
-set "baseOutputDir=C:\Users\naiar\Naidocking\outputs\"
+set "configDir=C:\Users\naiar\Naidocking\automatización\configs"
+set "ligandDir=C:\Users\naiar\Naidocking\automatización\ligands"
+set "baseOutputDir=C:\Users\naiar\Naidocking\automatización\outputs\"
 
 if not exist "%baseOutputDir%" mkdir "%baseOutputDir%"
 
 for %%c in (%configDir%*_config.txt) do (
     set "configFile=%%c"
     set "configName=%%~nc"
-    echo Procesando configuracion: !configName!
     
-    rem Quitar "_config" del nombre del archivo
-    set "baseName=!configName:_config=!"
+    rem extraer antes del "_config" del nombre del archivo
+    for /f "tokens=1 delims=_config" %%a in ("!fullName!") do (
+        set "configName=%%a"
+    )
+    echo Nombre base de la configuracion: !baseName!
+
     set "outputDir=!baseOutputDir!!configName!"
     if not exist "!outputDir!" mkdir "!outputDir!"
 
@@ -21,7 +24,7 @@ for %%c in (%configDir%*_config.txt) do (
         set "ligand=%%f"
         set "ligandName=%%~nf"
         echo Ejecutando docking: !ligandName! con config !configName!
-        "C:\Program Files (x86)\The Scripps Research Institute\Vina\vina.exe" --config "%%c" --ligand "%%f" --out "!outputDir!\!ligandName!_out.pdbqt"
+        "C:\Program Files (x86)\The Scripps Research Institute\Vina\vina.exe" --config "%%c" --ligand "%%f" --out "!outputDir!!configName!_ligandName!_out.pdbqt"
         echo Docking completado para: !ligandName! con configuracion !configName!
     )
 )
